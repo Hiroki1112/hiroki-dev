deeb4775a735cfb0439a8336009d6fb100dd6ed2
Hiroki1112 <pystdy@yahoo.co.jp>
2023-09-23 23:03:57 +0900

    ✨ Connect Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  output: 'server',
+  adapter: netlify(),
   integrations: [
     tailwind({
       config: {
