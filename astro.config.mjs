74faaed0266afde02ae88ddbfd2699c7ad93f147
Hiroki1112 <pystdy@yahoo.co.jp>
2023-09-23 23:50:09 +0900

    ✨ Connect to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  output: "server",
+  adapter: vercel(),
   integrations: [
     tailwind({
       config: {
